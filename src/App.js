51ee5c830254cdc4c44eb8abb226281d48c69555
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-12-06 17:48:39 +0530

    aalok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+import About from './components/About.js';
+
+import {BrowserRouter as Router, Routes, Route, Link, useParams} from 'react-router-dom'
+import Customerlist from './components/Customerlist.js';
+import PostForm from './components/PostForm.js';
+import EditUser from './components/EditUser.js';
+
 function App() {
+  const Wrapper = (props) => {
+    const params = useParams();
+    return <EditUser {...{...props, match: {params}} } />
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+    <Router>
+      <div className='App'>
+        <nav>
+          <ul>
+            <Link to="/">Home</Link> &nbsp;&nbsp;
+            <Link to="/customers">All Customers</Link> &nbsp;&nbsp;
+            <Link to="/add-customer">Add Customer</Link> &nbsp;&nbsp;           
+            <Link to="/about">About Us</Link> &nbsp;&nbsp;            
+          </ul>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<About />}></Route>
+          <Route exact path="/customers" element={<Customerlist />}></Route>
+          <Route exact path="/add-customer" element={<PostForm />}></Route>
+          <Route exact path="/edit-customer/:id" element={<Wrapper />}></Route>
+          <Route exact path="/about" element={<About />}></Route>          
+        </Routes>
+      </div>
+
+    </Router>
+      
     </div>
   );
 }
