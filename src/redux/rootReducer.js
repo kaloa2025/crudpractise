d9da0fb8c15d24272535d2fb024922dea9997386
kaloa2025 <aalokchoudhari2021@gmail.com>
2024-02-08 10:01:50 +0530

    redux

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import cakeReducer from './cake/cakeReducer';
+import iceCreamReducer from './iceCream/iceCreamReducer';
+const rootReducer  = combineReducers({
+    cake:cakeReducer,
+    icecream:iceCreamReducer,
+})
+export default rootReducer;
